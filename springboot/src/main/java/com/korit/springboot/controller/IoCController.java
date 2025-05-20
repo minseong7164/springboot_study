ac00662cd79c6a8f2242984e95bd3d247da87fa3
minseong7164 <minseong7164@naver.com>
2025-05-20 17:01:17 +0900

    feat:20250520/get,post

---
+++ springboot/src/main/java/com/korit/springboot/controller/IoCController.java
@@ -0,0 +1,31 @@
+package com.korit.springboot.controller;
+
+import com.korit.springboot.ioc.배터리;
+import com.korit.springboot.ioc.삼성배터리;
+import com.korit.springboot.ioc.스마트폰;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+//@AllArgsConstructor
+public class IoCController {
+
+    @Autowired      // 자동으로 객체를 생성
+    private 스마트폰 s;
+
+    @GetMapping("/ioc")
+    public String get() {
+        s.전원켜기();
+        return null;
+    }
+
+    @GetMapping("/ioc2")
+    public String get2() {
+        s.전원켜기();
+        return null;
+    }
+}
---
+++ springboot/src/main/java/com/korit/springboot/dto/Address.java
@@ -0,0 +1,15 @@
+package com.korit.springboot.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+    @Data
+    public class Address {
+        private String address1;
+        private String address2;
+        private String address3;
+        public List<String> address4;
+    }
+
